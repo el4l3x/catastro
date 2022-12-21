480a8205ecc539b28996606da125b23d23f1b9e1
el4l3x <davidlopez.fya@gmail.com>
2022-12-21 09:33:43 -0400

    Add Laravel Permission

--- config/app.php
+++ config/app.php
@@ -197,6 +197,9 @@ return [
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
 
+        /* Laravel Permission */
+        Spatie\Permission\PermissionServiceProvider::class,
+
     ],
 
     /*
